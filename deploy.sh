51cad67dcb8cc63a27f8d9d5a283500bf78efb8a
EN20200603\toby010203222 <toby010203222@staff.1111.com.tw>
2022-08-12 10:45:27 +0800

    自動部屬調整

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ npm run build
 cd dist
 
 git init
-git add -A
+git add .
 git commit -m 'deploy'
 
-git push -f https://github.com/whiskascats/prevention-map.git main:gh-pages
+git push -f https://github.com/whiskascats/prevention-map.git master:gh-pages
 
 cd - 
